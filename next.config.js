c34f3bac8f188b74de5bcf4211294dc3f64fd919
Daniel13greg <94358723+Daniel13greg@users.noreply.github.com>
2025-10-23 17:28:43 +0300

    Fix import resolution issues: update tsconfig paths, improve build configuration, add Node.js version specification

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'image.tmdb.org' },
+      { protocol: 'https', hostname: 'placehold.co' },
+      { protocol: 'https', hostname: 'i.imgur.com' },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', process.env.NEXT_PUBLIC_BASE_URL?.replace('https://', '').replace('http://', '')].filter(Boolean),
+    },
+  },
+};
+
+module.exports = nextConfig;
