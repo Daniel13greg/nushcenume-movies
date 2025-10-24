57dd3cf9d0d885fa2782f6b580caddb7e6c8dba0
Daniel13greg <94358723+Daniel13greg@users.noreply.github.com>
2025-10-24 18:21:47 +0300

    Add shadcn/ui components - accordion and alert-dialog

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import * * React from "react"
-import * * AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
+import * as React from "react"
+import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
